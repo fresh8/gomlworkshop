79eb283c9d05b74626d507aeb4194c0495a29131
Robert Jones <robert.g.jones@gmail.com>
2019-04-02 13:06:16 +0100

    Predictor can find distances and sort to find three closest

--- harness/harness.js
+++ harness/harness.js
@@ -1,103 +1,110 @@
-var fs = require('fs')
+var fs = require("fs");
 
 // Formatt CSV to JSON function.
 function formatCSV(filePath) {
   var dataArray = [];
   var fileContents = fs.readFileSync(filePath);
-  var lines = fileContents.toString().split('\n');
+  var lines = fileContents.toString().split("\n");
   // split lines from original string
   for (var i = 1; i < lines.length; i++) {
-	  dataArray.push(lines[i].toString().split(','));
+    dataArray.push(lines[i].toString().split(","));
   }
 
   // need to remove row delimitter
   for (let i = 0; i < dataArray.length; i++) {
-    dataArray[i][8] = dataArray[i][8].replace(/[\r\n]/g, "")
+    dataArray[i][8] = dataArray[i][8].replace(/[\r\n]/g, "");
   }
 
   // parse each data point to an integer
   for (let i = 0; i < dataArray.length; i++) {
-    for(let j = 0; j < dataArray[i].length; j++) {
-      dataArray[i][j] = parseInt(dataArray[i][j])
+    for (let j = 0; j < dataArray[i].length; j++) {
+      dataArray[i][j] = parseInt(dataArray[i][j]);
     }
   }
-  return dataArray
+  return dataArray;
 }
 
 // Splits data into training and testing sets, randomly selecting training entries.
 function split(baselineData) {
-  const trainingLength = Math.round((baselineData.length/100)*70)
-  let trainingData = []
-  let trainingLabels = []
-  let testingLabels = []
+  const trainingLength = Math.round((baselineData.length / 100) * 70);
+  let trainingData = [];
+  let trainingLabels = [];
+  let testingLabels = [];
   // make training set, by pulling random indexs from the baselineData.
   // this would leave random data left in the test data.
   for (i = 0; i < trainingLength; i++) {
-    trainingData.push(baselineData.splice(Math.round(Math.random()*10), 1)[0])
+    trainingData.push(
+      baselineData.splice(Math.round(Math.random() * 10), 1)[0]
+    );
   }
   // get training labels by removing from traning data.
   for (i = 0; i < trainingData.length; i++) {
-    trainingLabels.push(trainingData[i].splice(8, 1)[0])
+    trainingLabels.push(trainingData[i].splice(8, 1)[0]);
   }
   // get testing labels by removing from testing data.
   for (i = 0; i < baselineData.length; i++) {
-    testingLabels.push(baselineData[i].splice(8, 1)[0])
+    testingLabels.push(baselineData[i].splice(8, 1)[0]);
   }
   return {
     trainingData,
     testingData: baselineData,
     trainingLabels,
     testingLabels
-  }
+  };
 }
 
 // method to call to measure how accurate on your Knn algo.
 function evaluator(csvPath, algo) {
   // read data from csv and format
-  baseData = formatCSV(csvPath)
+  baseData = formatCSV(csvPath);
   // split data
-  let { trainingData, testingData, testingLabels, trainingLabels } = split(baseData)
+  let { trainingData, testingData, testingLabels, trainingLabels } = split(
+    baseData
+  );
 
-  algo.fit(trainingData, trainingLabels)
+  algo.fit(trainingData, trainingLabels);
 
-  const predictions = algo.predict(testingData)
+  const predictions = algo.predict(testingData);
 
-  return evaluate(predictions, testingLabels)
+  return evaluate(predictions, testingLabels);
 }
 
 // evaluates predicted results of testing vs traning data, returning 4 different types of accuracy/precision
 // metrics.
 function evaluate(predictions, labels) {
-  let truePos = 0, falseNeg = 0, falsePos = 0, trueNeg = 0
+  let truePos = 0,
+    falseNeg = 0,
+    falsePos = 0,
+    trueNeg = 0;
   for (i = 0; i < labels.length; i++) {
-    if(labels[i] === 1) {
-      if(predictions[i][0] === 1) {
-        truePos++
+    if (labels[i] === 1) {
+      if (predictions[i][0] === 1) {
+        truePos++;
       } else {
-        falseNeg++
+        falseNeg++;
       }
     } else {
       if (predictions[i][0] === 1) {
-        falsePos++
+        falsePos++;
       } else {
-        trueNeg++
+        trueNeg++;
       }
     }
   }
 
-  const accuracy = (trueNeg + truePos) / labels.length
-  const precision = truePos / (truePos + falsePos)
-  const recall = truePos / (truePos + falseNeg)
-  const f1 = 2 * ((precision * recall) / (precision + recall))
+  const accuracy = (trueNeg + truePos) / labels.length;
+  const precision = truePos / (truePos + falsePos);
+  const recall = truePos / (truePos + falseNeg);
+  const f1 = 2 * ((precision * recall) / (precision + recall));
 
   return {
     accuracy: accuracy,
     precision: precision,
     recall: recall,
     f1: f1
-  }
+  };
 }
 
 module.exports = {
   evaluator: evaluator
-}
+};
--- src/md-rj-index.js
+++ src/md-rj-index.js
@@ -1,3 +1,50 @@
-import '../harness/harness.js'
+const harness = require("../harness/harness.js");
 
+class Classifier {
+  constructor(knn) {
+    this.trainingData = [];
+    this.trainingLabels = [];
+    this.knn = knn;
+  }
+  fit(trainingData, trainingLabels) {
+    this.trainingData = trainingData;
+    this.trainingLabels = trainingLabels;
+  }
+  predict(testingData) {
+    const predictions = testingData.map((testRow, testIdx) => {
+      const distances = this.trainingData.map((trainRow, trainIdx) => {
+        return {
+          distance: calculateEuclideanDistance(testRow, trainRow),
+          index: trainIdx
+        };
+      });
+      distances.sort(sortDistances);
+      const knn = distances.splice(0, this.knn);
+      return knn;
+    });
+    return predictions;
+  }
+}
 
+const calculateEuclideanDistance = (arr1, arr2) => {
+  const sum = arr1.reduce(
+    (acc, data, idx, arr) => acc + Math.pow(data - arr2[idx], 2)
+  );
+  return Math.sqrt(sum);
+};
+
+const sortDistances = function(a, b) {
+  if (a.distance < b.distance) {
+    return -1;
+  }
+  if (a.distance > b.distance) {
+    return 1;
+  }
+  // a must be equal to b
+  return 0;
+};
+
+const algo = new Classifier(3);
+const result = harness.evaluator("../diabetes.csv", algo);
+
+console.log("RESULT", result);
